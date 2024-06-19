1bf39196467fc44ed85e175e5558edc7a40b37e5
John Stangel <jstangel@bellarmine.edu>
2024-06-18 19:58:23 -0500

    Updated branch with a java file.

---
+++ Assignment6/JRS_assignment3 - Copy/Assignment6.java
@@ -0,0 +1,9 @@
+public class Assignment6 {
+
+public static void main(String[] args) {
+
+System.out.println("I've updated this so that when I push this branch back it'll show  a change.");
+
+}
+
+}
